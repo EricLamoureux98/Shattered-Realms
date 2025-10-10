20964b0ecee922d312195bf82f9f60839f296cf9
Eric Lamoureux <ericandbailey@gmail.com>
2025-10-10 12:15:34 -0400

    Player Animation Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] Animator anim;
     [SerializeField] float movementSpeed;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private int facingDirection = 1;
 
     void Awake()
     {
@@ -16,6 +18,21 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = moveInput * movementSpeed;
+
+        // Checks direction player is facing in contrast to movement direction
+        if (moveInput.x > 0 && transform.localScale.x < 0 || moveInput.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(moveInput.x)); //MathF.Abs turns all # into positive
+        anim.SetFloat("vertical", Mathf.Abs(moveInput.y)); //Required to animate in both directions
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
     public void Move(InputAction.CallbackContext context)
