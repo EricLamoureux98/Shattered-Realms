4d8fcc366ab7d8d28fcdd11035c53072f98f615f
Eric Lamoureux <ericandbailey@gmail.com>
2025-11-06 07:55:51 -0500

    Stats UI

---
+++ Assets/Scripts/StatsUI.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StatsUI : MonoBehaviour
+{
+    [SerializeField] GameObject[] statsSlots;
+    [SerializeField] CanvasGroup statsCanvas;
+
+    bool statsOpen = false;
+
+    void Start()
+    {
+        UpdateAllStats();
+    }
+
+    public void UpdateDamage()
+    {
+        statsSlots[0].GetComponentInChildren<TMP_Text>().text = "Damage: " + StatsManager.Instance.damage;
+    }
+
+    public void UpdateSpeed()
+    {
+        statsSlots[1].GetComponentInChildren<TMP_Text>().text = "Speed: " + StatsManager.Instance.speed;
+    }
+
+    public void UpdateAllStats()
+    {
+        UpdateDamage();
+        UpdateSpeed();
+    }
+
+    public void ToggleStats(InputAction.CallbackContext context)
+    {
+        if (context.performed && statsOpen)
+        {
+            UpdateAllStats();
+            Time.timeScale = 1;
+            statsCanvas.alpha = 0;
+            statsOpen = false;
+        }
+        else if (context.performed && !statsOpen)
+        {
+            UpdateAllStats();
+            Time.timeScale = 0;
+            statsCanvas.alpha = 1;
+            statsOpen = true;
+        }
+    }
+}
